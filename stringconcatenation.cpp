a7c7672aec14c02f71f6dc3ac245df17999c6602
SharvariMurade16 <sharvari.murade.btech2023@sitpune.edu.in>
2024-08-23 10:25:16 +0530

    Create stringconcatenation.cpp

---
+++ stringconcatenation.cpp
@@ -0,0 +1,17 @@
+```
+//sharvari murade
+//23070123088
+//entc-b1
+
+#include <iostream>
+using namespace std;
+int main() {
+    string n, s;
+    cout<<"Enter your first name: ";
+    cin>>n;
+    cout<<"Enter your last name: ";
+    cin>>s;
+    cout<<"Your full name is: "<<n + s;
+return 0;
+}
+```
